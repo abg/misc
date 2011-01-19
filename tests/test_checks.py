b324dba2abecbc4db9e2645b1b249b6bdd3c6fe5
Andrew Garner <muzazzi@gmail.com>
2011-01-19 23:06:04 +0000

    flesh out tests

---
+++ tests/test_checks.py
@@ -0,0 +1,21 @@
+"""This module tests the builtin Configspec checks"""
+from nose.tools import *
+from config4py.checks import *
+
+def test_bool():
+    pass
+
+def test_int():
+    pass
+
+def test_float():
+    pass
+
+def test_string():
+    pass
+
+def test_list():
+    pass
+
+def test_cmdline():
+    pass
