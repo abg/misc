67ce193047c1a7470f5ea3978d04f0bee6daed38
Andrew Garner <muzazzi@gmail.com>
2011-01-19 23:05:19 +0000

    handle missing config keys in spec validation

--- config4py/spec.py
+++ config4py/spec.py
@@ -1,5 +1,6 @@
 """This module provides support for configs that validate other configs"""
 from config4py.config import Config
+from config4py.parsing import CheckParser
 from config4py.checks import builtin_checks
 
 class Configspec(Config):
@@ -12,11 +13,21 @@ class Configspec(Config):
     def validate(self, config):
         """Validate a config against this configspec"""
         for key, value in self.iteritems():
-            if isinstance(value, Configspec):
+            #XXX: value must be a Configspec if we want
+            #     to recurse to more than 1 level
+            if isinstance(value, dict):
                 # recurse to section
-                value.validate(config[key])
+                try:
+                    cfgsect = config[key]
+                except KeyError:
+                    cfgsect = config.setdefault(key, config.__class__())
+                value.validate(cfgsect)
             else:
                 name, args, kwargs = CheckParser.parse(value)
                 check = self.registry[name](*args, **kwargs)
-                config[key] = check.check(config[key])
+                try:
+                    value = config[key]
+                except KeyError:
+                    value = kwargs.get('default')
+                config[key] = check.check(value)
         return config
