37b81a896d344e440bc059177c09a7b0472a44c8
Andrew Garner <muzazzi@gmail.com>
2011-01-20 01:57:58 +0000

    add missing __version__ attribute

--- config4py/__init__.py
+++ config4py/__init__.py
@@ -1,5 +1,7 @@
 """Config4Py configuration management utility"""
 
+__version__ = '1.0.0a1'
+
 from config4py.config import Config
 from config4py.spec import Configspec
 from config4py.parsing import CheckParser
